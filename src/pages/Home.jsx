c299c15ebed7c7d1cf99a4a6e0b21f5d517057b3
“Ivan <ivanjunior2004@hotmail.com>
2026-02-05 21:53:13 -0300

    Atualizacao do Página Home - Banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,51 @@
+import styles from './Styles/Home.module.css'
+
 const Home = () => {
     return (
-         <h1>Bem vindo a FS11 Sports</h1>
+        <div >
+            
+            <div className={styles.banner}>
+                <div className={styles.tituloBanner}>Até 50% OFF</div>
+                <div className={styles.subTituloBanner}>A troca de temporada começou</div>
+                <button className={styles.botaoBanner}>APROVEITE</button>
+            </div>
+
+
+            <div className={styles.cards}>
+                <h2>Cards</h2>
+
+            </div>
+
+            <div className={styles.paraQuem}>
+                <h2>Para quem voce está comprando ??</h2>
+
+            </div>
+
+            <div className={styles.textoLoja}>
+
+                <div className={styles.divLoja}>
+                
+                <h2>LOJA DE ESPORTES ADIDAS PARA PERFORMANCE, ESTILO E INOVAÇÃO - DESDE 1949</h2>
+                <p>
+                O esporte mantém nossa forma. Mantém nosso foco. Nos une. Através dos esportes, temos o poder de mudar vidas - com histórias de atletas inspiradores, tecnologias inovadoras e sua participação ativa.
+Roupas esportivas de alta performance feitas com as mais recentes tecnologias para te ajudar a superar
+Limites. Seja você corredor, jogador de basquete, de futebol ou alguém que valoriza se manter ativo, nossa loja de esportes é a sua casa. Estamos aqui para ajudá-lo a treinar mais, correr mais rápido e ir mais longe, com materiais esportivos que acompanham seus movimentos e apoiam seus objetivos.
+Explore nossa loja online para as mais recentes coleções adidas, quando a inovação encontra o estilo. Do coach de yoga espalhando o bem-estar do movimento até quem escolhe fugir da cidade no final de semana, apoiamos todos que se esforçam para dar o seu melhor. A loja de esportes da adidas oferece equipamentos de alto desempenho para todos os níveis, seja você um atleta de elite, um entusiasta do fitness ou apenas um iniciante dessa jornada. Nossa loja online garante acesso aos últimos lançamentos feitos para seu movimento, aonde quer que você va.
+A loja de esportes da adidas é mais do que um lugar para comprar. Cada item da nossa loja de esporte é projetado com desempenho, durabilidade e atenção aos detalhes em mente, ajudando você a se movimentar com confiança. Na adidas, temos parcerias com os melhores do setor para criar colaborações, garantindo que cada peça de roupa apoie o movimento, a autoexpressão e um estilo de vida ativo. Aonde quer que o seu esporte te leve, nossa loja de esportes te dá suporte, ajudando você a dar cada passo em direção ao seu melhor.
+                </p>
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+
+        
+       
+
+         
     )
 }
 
